3f33f0f0d1bfd7e55a7fe07329d9b93af4b16e06
Maxim Shashurin <waiyyshik@gmail.com>
2021-02-28 22:16:24 +0500

    фокус на первом поле ввода при открытии .modal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const btnWriteUs = document.querySelector('.contacts-button')
-const closeModal = document.querySelector('.modal-close')
 const modal = document.querySelector('.modal')
+const closeModal = modal.querySelector('.modal-close')
+const nameInput = modal.querySelector('[name = name]')
 
-function toggleHidden(evt){
+btnWriteUs.addEventListener('click', function(evt){
   evt.preventDefault()
-  modal.classList.toggle('hidden')
-}
+  modal.classList.remove('hidden')
+  nameInput.focus()
+})
 
-btnWriteUs.addEventListener('click', toggleHidden)
-closeModal.addEventListener('click',toggleHidden)
+closeModal.addEventListener('click', function () {
+  modal.classList.add('hidden')
+})
