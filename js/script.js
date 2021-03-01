09e4fbe50cad575bd2d79e020b0c5628f7be92d1
Maxim Shashurin <waiyyshik@gmail.com>
2021-03-01 23:29:39 +0500

    сделал js но работает не очень корректно

--- js/script.js
+++ js/script.js
@@ -1,14 +1,55 @@
-const btnWriteUs = document.querySelector('.contacts-button')
-const modal = document.querySelector('.modal')
-const closeModal = modal.querySelector('.modal-close')
-const nameInput = modal.querySelector('[name = name]')
+const btnWriteUs = document.querySelector(".contacts-button");
+const modal = document.querySelector(".modal");
+const closeModal = modal.querySelector(".modal-close");
+const form = modal.querySelector(".modal-form");
+const nameInput = form.querySelector("[name = name]");
+const emailInput = form.querySelector("[name = email]");
+let isStorageSupport = true;
+const storage = localStorage.getItem("name");
 
-btnWriteUs.addEventListener('click', function(evt){
-  evt.preventDefault()
-  modal.classList.remove('hidden')
-  nameInput.focus()
-})
+try {
+  storage = localStorage.getItem("name");
+} catch (error) {
+  isStorageSupport = false;
+}
 
-closeModal.addEventListener('click', function () {
-  modal.classList.add('hidden')
-})
+btnWriteUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("hidden");
+  if (storage) {
+    nameInput.value = storage;
+  }
+  if (nameInput.value == storage) {
+    emailInput.focus();
+  } else {
+    nameInput.focus();
+  }
+});
+
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  modal.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!nameInput.value || !emailInput.value) {
+    evt.preventDefault();
+    modal.classList.add("modal-error");
+    if (!nameInput.value) {
+      nameInput.classList.add("is-empty");
+    } else {
+      emailInput.classList.add("is-empty");
+    }
+  } else {
+    localStorage.setItem("name", nameInput.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27) {
+    if (!modal.classList.contains("hidden")) {
+      modal.classList.add("hidden");
+      modal.classList.add("modal-error");
+    }
+  }
+});
